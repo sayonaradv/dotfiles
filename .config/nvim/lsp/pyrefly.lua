c31c9c52e0f5c0b69968984f41f9b1d8cf10b4e3
shinigami-dv <107803920+shinigami-dv@users.noreply.github.com>
2025-08-19 19:54:48 +0100

    chore: stylua

--- .config/nvim/lsp/pyrefly.lua
+++ .config/nvim/lsp/pyrefly.lua
@@ -1,13 +1,11 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { "pyrefly", "lsp" },
-    root_markers = {
-        "pyrefly.toml",
-        "pyproject.toml",
-        ".git",
-    },
-    filetypes = { "python" },
-    on_exit = function(code, _, _)
-        vim.notify('Closing Pyrefly LSP exited with code: ' .. code, vim.log.levels.INFO)
-    end,
+  cmd = { "pyrefly", "lsp" },
+  root_markers = {
+    "pyrefly.toml",
+    "pyproject.toml",
+    ".git",
+  },
+  filetypes = { "python" },
+  on_exit = function(code, _, _) vim.notify("Closing Pyrefly LSP exited with code: " .. code, vim.log.levels.INFO) end,
 }
