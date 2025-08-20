d523dca56990886e6fadcdbe3ca5416decdb9938
shinigami-dv <107803920+shinigami-dv@users.noreply.github.com>
2025-08-20 15:20:03 +0100

    fix: improve blink completition

--- .config/nvim/plugin/+plugins.lua
+++ .config/nvim/plugin/+plugins.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main" },
   { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
+  { src = "https://github.com/onsails/lspkind.nvim" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/dmtrKovalenko/fff.nvim" },
@@ -39,11 +40,47 @@ require("blink.cmp").setup({
   appearance = {
     nerd_font_variant = "mono",
   },
-  completion = { documentation = { auto_show = false } },
   sources = {
     default = { "lsp", "path", "snippets", "buffer" },
   },
   fuzzy = { implementation = "prefer_rust_with_warning" },
+  completion = {
+    documentation = { auto_show = false },
+
+    menu = {
+      draw = {
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              local icon = ctx.kind_icon
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then icon = dev_icon end
+              else
+                icon = require("lspkind").symbolic(ctx.kind, {
+                  mode = "symbol",
+                })
+              end
+
+              return icon .. ctx.icon_gap
+            end,
+
+            -- Optionally, use the highlight groups from nvim-web-devicons
+            -- You can also add the same function for `kind.highlight` if you want to
+            -- keep the highlight groups in sync with the icons.
+            highlight = function(ctx)
+              local hl = ctx.kind_hl
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then hl = dev_hl end
+              end
+              return hl
+            end,
+          },
+        },
+      },
+    },
+  },
 })
 
 require("fff").setup({
