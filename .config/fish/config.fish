da7f96eb516c3be6abd461bf74caa99a1b67c0aa
shinigami-dv <107803920+shinigami-dv@users.noreply.github.com>
2025-08-25 16:09:13 +0100

    feat: add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+end
+
+set -gx EDITOR nvim
+set -Ux XDG_CONFIG_HOME $HOME/.config
+set -U fish_greeting
+
+alias v="nvim"
+alias lg="lazygit"
+
+# starship init fish | source
+zoxide init fish | source
